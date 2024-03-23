df0f9a1fb897ee9e245e501bf6fe26f10f394d44
ipullynnhah <ipullynnhah@gmail.com>
2024-03-23 01:16:43 -0300

    :white_check_mark: feat: implement function buscaCortePorId

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 const barbearia = {
-    cortes: [
-        { id: 1, tipo: "Militar", valor: 20 },
-        { id: 2, tipo: "Samurai", valor: 35 },
-        { id: 3, tipo: "Pompadour", valor: 20 },
-        { id: 4, tipo: "Moicano", valor: 10 },
-        { id: 5, tipo: "Razor part", valor: 5 },
-    ],
-  
-    barbas: [
-        { id: 1, tipo: "Capitão Jack", valor: 20 },
-        { id: 2, tipo: "Van Dyke", valor: 20 },
-        { id: 3, tipo: "Barba Média", valor: 20 },
-        { id: 4, tipo: "Barba Baixa", valor: 20 },
-        { id: 5, tipo: "Barba Zero", valor: 15 },
-    ],
-    estaAberto: true,
+  cortes: [
+    { id: 1, tipo: "Militar", valor: 20 },
+    { id: 2, tipo: "Samurai", valor: 35 },
+    { id: 3, tipo: "Pompadour", valor: 20 },
+    { id: 4, tipo: "Moicano", valor: 10 },
+    { id: 5, tipo: "Razor part", valor: 5 },
+  ],
+
+  barbas: [
+    { id: 1, tipo: "Capitão Jack", valor: 20 },
+    { id: 2, tipo: "Van Dyke", valor: 20 },
+    { id: 3, tipo: "Barba Média", valor: 20 },
+    { id: 4, tipo: "Barba Baixa", valor: 20 },
+    { id: 5, tipo: "Barba Zero", valor: 15 },
+  ],
+  estaAberto: true,
 };
-  
-  
+
 function buscaCortePorId(id) {
+  const { cortes } = barbearia;
+  for (let i = 0; i < cortes.length; i++) {
+    if (cortes[i].id === id) {
+      return cortes[i];
+    }
+  }
+
+  return "Corte não encontrado";
 }
 
-function buscaBarbaPorId(id) {
-}
+function buscaBarbaPorId(id) {}
 
-function verificaStatusBarbearia() {
-}
+function verificaStatusBarbearia() {}
 
-function retornaTodosCortes() {
-}
+function retornaTodosCortes() {}
 
-function retornaTodasBarbas() {
-}
+function retornaTodasBarbas() {}
 
-function criaPedido(nomeCliente, corteId, barbaId) {
-}
+function criaPedido(nomeCliente, corteId, barbaId) {}
 
-function atualizarServico(lista, id, valor, tipo) {
-}
+function atualizarServico(lista, id, valor, tipo) {}
 
-function calculaTotal(pedido) {
-}
-  
-  
+function calculaTotal(pedido) {}
